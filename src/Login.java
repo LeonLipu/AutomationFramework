d65e1c6f24fa6ebfa66d56135c6b4aeb775e38a7
brkar <brkar>
2016-11-28 14:35:24 +0530

    Initial commit

---
+++ src/Login.java
@@ -0,0 +1,19 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+/**
+ * Created by brkar on 11/25/2016.
+ */
+public class Login {
+
+    @Test
+    public void getname(){
+        System.out.println("Myname is brahmananda kar");
+        System.setProperty("webdriver.chorme.driver","chrome.exe");
+        WebDriver driver=new ChromeDriver();
+        driver.close();
+
+
+    }
+}
